f6154040d6b256e15a785eeabdbddc05badcb1d2
ebegoflo <ebenezergf@gmail.com>
2018-04-24 13:00:47 -0500

    consumiendo data y pintando

---
+++ js/vendors/afore.js
@@ -0,0 +1,40 @@
+let getInfo  = (data,userName) => {
+    let user = data ['userInfo']['user_name'];
+    let accountResume= data['accountGroupSummary'];
+    accountResume.forEach(function(element) {
+        let account= element.accounts;
+            account.forEach(function(array){
+                let savingsAccountSummary = array.savingsAccountSummary;
+                let status = savingsAccountSummary.accountStatus;
+                let balance = savingsAccountSummary.availableBalance;
+                let accountNumber = savingsAccountSummary.displayAccountNumber;
+
+                // console.log(status);
+                // console.log(balance);
+                // console.log(accountNumber);
+                paintAccout(status,balance,accountNumber);  
+            })
+    });
+   
+  }
+  
+function paintAccout (status,balance,accountNumber){
+
+    const movieContainer = document.getElementById('afore_Container');
+
+    let templete = `<div class="col s12 m4">
+          <div>
+            <div>
+                <p> Numero de cuenta ${accountNumber}<p>
+              <p> El estatus de tu cuenta: ${status}<p>
+              <p> Saldo afore: $${balance} MXN<p>
+             
+              <a href="./puntos.html">Añadir mis puntos Premia</a>
+            </div>
+            </div>
+        </div>`
+        movieContainer.innerHTML += templete;
+  }
+
+  getInfo(data);
+  
